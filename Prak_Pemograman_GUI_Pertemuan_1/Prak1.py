0f83b0f30984ad6c902e5dc128eba61ecf91dc6a
Yolanda Febrianti Silalahi <43991221+yolandasilalahi4@users.noreply.github.com>
2019-04-15 22:36:42 +0700

    Add files via upload

---
+++ Prak_Pemograman_GUI_Pertemuan_1/Prak1.py
@@ -0,0 +1,12 @@
+karakter = 'Jon Snow'
+price = 10
+x = 7
+y = 8
+z = 2
+
+print('karakter = ',karakter)
+print('price = ',price)
+print('x = ',x)
+print('y = ',y)
+print('z = ',z)
+print('(x*y)**z = ',(x*y)**z)
